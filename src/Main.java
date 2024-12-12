25f5eabc753e90fbe613f2fc0eb516b6f23a122c
EvaShaban <eva2022princess@gmail.com>
2024-12-12 20:19:04 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,7 @@ public class Main {
 
 
 
-
-    public static void main(String[] args) {
-        printThreeWords();
-
-
+    public static void checkSumSign() {
         int num1 = 6;
         int num2 = -100;
         int num3 = num1+num2;
@@ -27,7 +23,10 @@ public class Main {
         else if(num3<0){
             System.out.println("Сумма отрицательная");
         }
+    }
 
+
+    public static void printColor() {
         int value = 100;
         if(value>=101){
             System.out.println("Зеленый");
@@ -38,8 +37,9 @@ public class Main {
         else {
             System.out.println("Желтый");
         }
+    }
 
-
+    public static void compareNumbers() {
         int a = 50;
         int b = 50;
 
@@ -52,9 +52,9 @@ public class Main {
         else if(a==b){
             System.out.println("a равно b");
         }
+    }
 
-
-
+    public static void summ() {
         int x = 50;
         int y = 10;
 
@@ -64,9 +64,9 @@ public class Main {
         else {
             System.err.println("false");
         }
+    }
 
-
-
+    public static void zero1() {
         int[] arr1 = {0, -2, 4, 10, -22, 35, -14, 45};
         int pos = 0;
         int neg = 0;
@@ -95,31 +95,32 @@ public class Main {
 
         System.out.println(Arrays.toString(arrneg) + " Отрицательные числа ");
         System.out.println(Arrays.toString(arrpos) + " Положительные числа ");
+    }
 
 
-
-
+    public static void True1() {
         Number[] nums = {10,-5,3,48,50, 0};
+
         for (Number num : nums) {
             System.out.println(num + " Является ли значение отрицательным? Ответ: " + isNegative(num));
-
         }
-        public static <T extends Number> boolean isNegative(T num) {
+    }
+
+    public static <T extends Number> boolean isNegative(T num) {
         return num.intValue() < 0;
-        }
+    }
 
 
-
-
+    public static void count1 () {
 
         String string="abc";
+
         int count=3;
+
         System.out.println("String :"+string.repeat(count));
 
-
-
-
-
+    }
+    public static void year1 () {
         boolean a = true;
         boolean b = false;
         int year = 2024;
@@ -129,10 +130,9 @@ public class Main {
         else {
             System.out.println(b);
         }
+    }
 
-
-
-
+    public static void massiv() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 
         for (int i = 0; i < arr.length; i++) {
@@ -142,64 +142,92 @@ public class Main {
                 arr[i]--;
         }
         System.out.println(Arrays.toString(arr));
-
-
-
+    }
+    public static void pustoi() {
         int[] array = new int[100];
         for (int i = 0; i < 100; i++) {
             array[i] = i + 1;
             System.out.println(array[i]);
         }
-
-
-
-
-        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <6)
-                arr[i]*=2;
-
-        }
-
-        System.out.println(Arrays.toString(arr));
-
-        int[][] arr = new int[5][5];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i][i] = 1;
-            arr[i][arr.length - i - 1] = 1;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-
-
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите длину массива: ");
-        int len = scanner.nextInt();
-
-        int[] array = new int[len];
-
-        for (int i = 0; i < len; i++) {
-            System.out.print("Введите элемент " + (i + 1) + ": ");
-            array[i] = scanner.nextInt();
-        }
-
-        System.out.println("Элементы массива:");
-        for (int i : array) {
-            System.out.println(i);
-
-
-
-        }
-        }
-
     }
+
+        public static void massiv2() {
+            int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] <6)
+                    arr[i]*=2;
+
+            }
+
+            System.out.println(Arrays.toString(arr));
+        }
+
+public static void kvadrat() {
+
+    int[][] arr = new int[5][5];
+    for (int i = 0; i < arr.length; i++) {
+        arr[i][i] = 1;
+        arr[i][arr.length - i - 1] = 1;
+    }
+    for (int i = 0; i < arr.length; i++) {
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+    }  }
+
+
+public static void len1() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Введите длину массива: ");
+    int len = scanner.nextInt();
+
+    int[] array = new int[len];
+
+    for (int i = 0; i < len; i++) {
+        System.out.print("Введите элемент " + (i + 1) + ": ");
+        array[i] = scanner.nextInt();
+    }
+
+    System.out.println("Элементы массива:");
+    for (int i : array) {
+        System.out.println(i);
+    }
+
 }
+
+
+
+
+public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        summ();
+        zero1();
+        True1();
+        count1();
+        year1 ();
+        massiv();
+        pustoi();
+        massiv2();
+        kvadrat();
+        len1();
+
+
+
+
+
+
+}
+
+
+}
+
+
 
 
 
